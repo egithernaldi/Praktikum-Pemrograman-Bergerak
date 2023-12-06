1a1d228420b753e35673e4c77bcd8d4d8761cf47
Egit Hernaldi <114506105+egithernaldi@users.noreply.github.com>
2023-12-06 18:05:05 +0700

    Jobsheet1

---
+++ app/src/main/java/com/example/praktikum1/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.praktikum1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.hello_world)
+
+        val nameEditText: EditText = findViewById(R.id.nameEditText)
+        val buttonButton: Button = findViewById(R.id.buttonButton)
+        val buttonTextView: TextView = findViewById(R.id.buttonTextView)
+
+        buttonTextView.text = "Hallo"
+
+        buttonButton.setOnClickListener{
+            val name = nameEditText.text.toString()
+            buttonTextView.text = "Hallo $name"
+        }
+    }
+}
