ce8d34d53371c75a5b5e2aab603ae43f01a8cc05
Egit Hernaldi <114506105+egithernaldi@users.noreply.github.com>
2023-12-06 18:15:34 +0700

    Jobsheet3

---
+++ app/src/main/java/com/example/praktikum3/Game.kt
@@ -0,0 +1,29 @@
+package com.example.praktikum3
+
+import kotlin.random.Random
+
+object Game {
+
+    private val rules = mapOf(
+        "ROCK-SCISSORS" to true,
+        "ROCK-PAPER" to true,
+        "SCISSORS-PAPER" to true,
+        "SCISSORS-ROCK" to false,
+        "PAPER-ROCK" to false,
+        "PAPER-SCISSORS" to false
+    )
+
+    private val options = listOf("ROCK", "SCISSORS", "PAPER")
+
+    private val optionDrawable = mapOf(
+        "ROCK" to R.drawable.rock,
+        "SCISSORS" to R.drawable.scissors,
+        "PAPER" to R.drawable.paper,
+    )
+    fun pickRandomOption(): String = options[Random.nextInt(0, 3)]
+    fun pickDrawable(option: String): Int = optionDrawable[option]!!
+    fun isDraw(from: String, to: String): Boolean = from == to
+    fun isWin(from: String, to: String): Boolean = rules["$from-$to"]!!
+
+
+}
